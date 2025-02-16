1ce7d37f04c83c4085dc016c30383eeb6e74c765
Michael-lthtr <167223110+Michael-lthtr@users.noreply.github.com>
2025-02-16 02:55:06 +0000

    removed commented lines

--- stroma/models/aurum/gold__observation.sql
+++ stroma/models/aurum/gold__observation.sql
@@ -55,27 +55,3 @@ SELECT
 /* o.unique_key, */ /* o.datasource, */ /* o.updated_at */
 FROM stg_gold.stg__observation AS o 
 
-/*SELECT
-  o.observation_id,
-  o.person_id,
-  o.observation_concept_id,
-  /*o.observation_date,
-  o.observation_datetime,
-  o.observation_type_concept_id,
-  o.value_as_number,
-  o.value_as_string,
-  o.value_as_concept_id,
-  o.qualifier_concept_id,
-  o.unit_concept_id,
-  o.provider_id,
-  o.visit_occurrence_id,
-  o.visit_detail_id,
-  o.observation_source_value,
-  o.observation_source_concept_id,
-  o.unit_source_value,
-  o.qualifier_source_value,
-  o.value_source_value,
-  o.observation_event_id,
-  o.obs_event_field_concept_id
-  /* o.unique_key, */ /* o.datasource, */ /* o.updated_at */ */
-FROM stg_gold.stg__observation AS o */
