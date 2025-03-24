09eb957a9af07de6020c1ba429671db73bb56632
trisdrobinson <150181156+trisdrobinson@users.noreply.github.com>
2025-03-24 10:56:41 +0000

    Update sqlmesh_runner.py (#27)

--- sqlmeshrunner/sqlmesh_runner.py
+++ sqlmeshrunner/sqlmesh_runner.py
@@ -6,8 +6,11 @@ import os
 # COMMAND ----------
 
 # DBTITLE 1,Get Secrets
+os.environ['DATABRICKS_STATE_DB_HOST'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "ConnectionStringTo-SqlMeshStateDatabase")
 os.environ['DATABRICKS_STATE_DB_USER'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UsernameTo-SqlMeshStateDatabase")
 os.environ['DATABRICKS_STATE_DB_PASSWORD'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "PasswordTo-SqlMeshStateDatabase")
+os.environ['DATABRICKS_SERVER_HOSTNAME'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UrlTo-DatabricksWorkspace")
+os.environ['DATABRICKS_HTTP_PATH'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "SqlEndpointTo-DatabricksWorkspace")
 os.environ['DATABRICKS_ACCESS_TOKEN'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "TokenTo-DatabricksAPI")
 
 # COMMAND ----------
@@ -15,14 +18,11 @@ os.environ['DATABRICKS_ACCESS_TOKEN'] = dbutils.secrets.get(scope = "AzureKeyVau
 os.environ['ENABLED_GATEWAYS']="databricks"
 os.environ['DEFAULT_GATEWAY']='databricks'
 os.environ['STATE_SCHEMA']="stroma"
-os.environ['DATABRICKS_SERVER_HOSTNAME']="adb-1868816176859647.7.azuredatabricks.net"
-os.environ['DATABRICKS_HTTP_PATH']="/sql/1.0/warehouses/0aec44b2e70e201d" # X-small
-os.environ['DATABRICKS_CATALOG']="rde_dev"
-os.environ['DATABRICKS_CATALOG_SOURCE']="dbw-lsc-uks-datadev-dlz-01"
+os.environ['DATABRICKS_CATALOG']="rde_development"
+os.environ['DATABRICKS_CATALOG_SOURCE']="dbw-lsc-uks-dataprd-dlz-01"
 os.environ['DATABRICKS_SCHEMA_BASE']="lth_omop"
 os.environ['DATABRICKS_SCHEMA_VOCAB']="lth_omop"
 os.environ['DATABRICKS_CONCURRENT_TASKS']='8'
-os.environ['DATABRICKS_STATE_DB_HOST']="lscsdeprod-psql.postgres.database.azure.com"
 os.environ['DATABRICKS_STATE_DB_PORT']='5432'
 os.environ['DATABRICKS_STATE_DB_DATABASE']="sqlmesh_stroma"
 
