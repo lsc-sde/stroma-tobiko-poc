813c250ae8f816831f31b84153bc15de8f238048
Michael-lthtr <167223110+Michael-lthtr@users.noreply.github.com>
2025-04-16 14:41:36 +0100

    successful run.

--- stroma/models/dummy_model.sql
+++ stroma/models/dummy_model.sql
@@ -17,4 +17,4 @@ MODEL (
 
 /* command to generate unit test code in a yaml file */ /* sqlmesh create_test tcloud_demo.incremental_model --query tcloud_demo.seed_model "select * from tcloud_demo.seed_model limit 5" */
 SELECT
-  1 AS item_id
+  2 AS item_id
